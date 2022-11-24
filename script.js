378e9627ae31ae768171ad23025d2d4e2377aac1
Jonatha Moreira <johnninajoao@gmail.com>
2022-11-24 10:56:30 -0300

    adição de botão interativo

---
+++ script.js
@@ -0,0 +1,11 @@
+function alerta() {
+
+    const liso = document.getElementById('btn')
+    if (liso) {
+        liso.addEventListener =
+            document.getElementById('julius').style.display = ('flex' || 'none')
+            document.getElementById('form').style.display = 'none'
+            document.getElementById('back').style.display = 'flex'
+            //event.preventDefault() 
+        }
+}
